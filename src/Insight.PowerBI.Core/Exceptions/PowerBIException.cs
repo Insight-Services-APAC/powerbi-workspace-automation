d8fd7694b84c504c57783ce1ed506d2adf14750f
Paul Smithdale <8188140+mrsmitty@users.noreply.github.com>
2021-06-21 17:01:19 +0800

    Fix nullable reference type

--- src/Insight.PowerBI.Core/Exceptions/PowerBIException.cs
+++ src/Insight.PowerBI.Core/Exceptions/PowerBIException.cs
@@ -6,6 +6,6 @@ namespace Insight.PowerBI.Core.Exceptions
 {
     public class PowerBIException : Exception
     {
-        public PowerBIException(string message, Exception? innerException = null) : base(message, innerException) { }
+        public PowerBIException(string message, Exception innerException = null) : base(message, innerException) { }
     }
 }
