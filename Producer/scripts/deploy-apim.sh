4e0a604fd1ae932127d68ea9971db0c88d25a5ab
Paul Smithdale <8188140+mrsmitty@users.noreply.github.com>
2021-06-28 16:45:43 +0800

    again

--- Producer/scripts/deploy-apim.sh
+++ Producer/scripts/deploy-apim.sh
@@ -4,18 +4,18 @@ echo "*********VARIABLES*********"
 CONTAINER="templates"
 REMOTE_TEMPLATE_PATH="temp"
 # APIM Variables
-echo "APIM Name: $(apim)"
-echo "Resource Group: $(rg)"
-echo "Storage Account: $(storage)"
-echo "Function App: $(func)"
+echo "APIM Name: $(APIM_NAME)"
+echo "Resource Group: $(RESOURCE_GROUP)"
+echo "Storage Account: $(STORAGE_ACCOUNT)"
+echo "Function App: $(FUNCTION_APP)"
 echo "Template Path: $(LOCAL_TEMPLATE_DIRECTORY)"
-ROOT_FILE_URL="https://$(storage).blob.core.windows.net/$CONTAINER/$REMOTE_TEMPLATE_PATH"
+ROOT_FILE_URL="https://$(STORAGE_ACCOUNT).blob.core.windows.net/$CONTAINER/$REMOTE_TEMPLATE_PATH"
 echo "Remote Template URL: $ROOT_FILE_URL"
 
 echo "*********CONNECTION*********"
 CONNECTION=$(az storage account show-connection-string \
-    --resource-group $(rg) \
-    --name $(storage) \
+    --resource-group $(RESOURCE_GROUP) \
+    --name $(STORAGE_ACCOUNT) \
     --query connectionString)
 
 
@@ -31,10 +31,10 @@ output=$(az storage blob upload-batch \
 
 
 echo "*********DEPLOYMENT VARIABLES*********"
-sed -i "s/test-syd-dev-fun-powerbi/$(func)/g" LOCAL_TEMPLATE_DIRECTORY/policies/*.xml
+sed -i "s/test-syd-dev-fun-powerbi/$(FUNCTION_APP)/g" LOCAL_TEMPLATE_DIRECTORY/policies/*.xml
 
-$FUNC_KEY = $(az functionapp keys list --name $(func) `
-    --resource-group $(rg) `
+$FUNC_KEY = $(az functionapp keys list --name $(FUNCTION_APP) `
+    --resource-group $(RESOURCE_GROUP) `
     --query "functionKeys.default" -o tsv)
 NAMED_VALUES = "{\"hsssyddevfunpowerbikey\": \"$FUNC_KEY\"}"
 
@@ -47,7 +47,7 @@ END=`date -u -d "30 minutes" '+%Y-%m-%dT%H:%MZ'`
 SAS=`az storage container generate-sas -n $CONTAINER --https-only --permissions dlrw --expiry $END --connection-string $CONNECTION -o tsv`
 
 az deployment group create \
-  --resource-group $(rg) \
+  --resource-group $(RESOURCE_GROUP) \
   --template-uri "$ROOT_FILE_URL/ApimArm-master.template.json?$SAS" \
   --parameters "ApimServiceName=$APIM_NAME" \
   --parameters "LinkedTemplatesBaseUrl=$ROOT_FILE_URL" \
@@ -55,5 +55,5 @@ az deployment group create \
   --parameters "PolicyXMLBaseUrl=$ROOT_FILE_URL/policies" \
   --parameters "PolicyXMLSasToken=?$SAS" \
   --parameters "NamedValues=$NAMED_VALUES" \
-  --parameters "FunctionBackendName=$(func)" \
+  --parameters "FunctionBackendName=$(FUNCTION_APP)" \
   --verbose
