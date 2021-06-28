11dc24bdd7ea403671ed57f5f62c4f97910adf1a
Paul Smithdale <8188140+mrsmitty@users.noreply.github.com>
2021-06-28 16:35:43 +0800

    try?

---
+++ Producer/scripts/deploy-apim.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo "*********VARIABLES*********"
+CONTAINER="templates"
+REMOTE_TEMPLATE_PATH="temp"
+# APIM Variables
+echo "APIM Name: $(apim)"
+echo "Resource Group: $(rg)"
+echo "Storage Account: $(storage)"
+echo "Function App: $(func)"
+echo "Template Path: $(LOCAL_TEMPLATE_DIRECTORY)"
+ROOT_FILE_URL="https://$(storage).blob.core.windows.net/$CONTAINER/$REMOTE_TEMPLATE_PATH"
+echo "Remote Template URL: $ROOT_FILE_URL"
+
+echo "*********CONNECTION*********"
+CONNECTION=$(az storage account show-connection-string \
+    --resource-group $(rg) \
+    --name $(storage) \
+    --query connectionString)
+
+
+
+echo "*********UPLOAD*********"
+output=$(az storage blob upload-batch \
+    --destination $CONTAINER \
+    --source "$LOCAL_TEMPLATE_DIRECTORY/" \
+    --destination-path $REMOTE_TEMPLATE_PATH \
+    --connection-string $CONNECTION)
+
+
+
+
+echo "*********DEPLOYMENT VARIABLES*********"
+sed -i "s/test-syd-dev-fun-powerbi/$(func)/g" LOCAL_TEMPLATE_DIRECTORY/policies/*.xml
+
+$FUNC_KEY = $(az functionapp keys list --name $(func) `
+    --resource-group $(rg) `
+    --query "functionKeys.default" -o tsv)
+NAMED_VALUES = "{\"hsssyddevfunpowerbikey\": \"$FUNC_KEY\"}"
+
+
+
+
+echo *********DEPLOYMENT*********"
+
+END=`date -u -d "30 minutes" '+%Y-%m-%dT%H:%MZ'`
+SAS=`az storage container generate-sas -n $CONTAINER --https-only --permissions dlrw --expiry $END --connection-string $CONNECTION -o tsv`
+
+az deployment group create \
+  --resource-group $(rg) \
+  --template-uri "$ROOT_FILE_URL/ApimArm-master.template.json?$SAS" \
+  --parameters "ApimServiceName=$APIM_NAME" \
+  --parameters "LinkedTemplatesBaseUrl=$ROOT_FILE_URL" \
+  --parameters "LinkedTemplatesSasToken=?$SAS" \
+  --parameters "PolicyXMLBaseUrl=$ROOT_FILE_URL/policies" \
+  --parameters "PolicyXMLSasToken=?$SAS" \
+  --parameters "NamedValues=$NAMED_VALUES" \
+  --parameters "FunctionBackendName=$(func)" \
+  --verbose
