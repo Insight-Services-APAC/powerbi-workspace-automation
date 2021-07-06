169202d3b76ab636655ba3e57a504301301215c5
van Aswegen <Willem.vanAswegen@insight.com>
2021-07-06 14:36:25 +0800

    Updates to deployment scripts

---
+++ Subscriber/PowerBITelemetryDB/CEN/Views/vw_Reports.sql
@@ -0,0 +1,23 @@
+﻿
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+ 
+ CREATE VIEW [CEN].[vw_Reports]
+ AS
+ SELECT [Report_Id]
+      ,[Name]
+      ,[WebUrl]
+      ,[EmbedUrl]
+      ,[Dataset_Id]
+      ,[Description]
+      ,[CreatedBy]
+      ,[ModifiedBy]
+      ,[CreatedDateTime]
+      ,[ModifiedDateTime]
+      ,[EndorsementDetails]
+      ,[SensitivityLabel]
+      ,[ReportType]
+      ,[Workspace_Id]
+      ,[DateRetrieved]
+  FROM [CEN].[Reports]
+  WHERE SUBSTRING(TRIM([Name]),1,5) <> '[App]'
--- Subscriber/PowerBITelemetryDB/CEN/Views/vw_Workspaces.sql
+++ Subscriber/PowerBITelemetryDB/CEN/Views/vw_Workspaces.sql
@@ -1,19 +1,51 @@
-﻿Create View [CEN].[vw_Workspaces]
-As
-Select  Workspace_ID
-       ,Name  
-       ,IsReadOnly  
-       ,IsOnDedicatedCapacity  
-       ,CapacityId  
-       ,Description  
-       ,Type  
-       ,State  
-       ,IsOrphaned  
-       ,Users  
-       ,Reports  
-       ,Dashboards  
-       ,Datasets  
-       ,Dataflows  
-       ,Workbooks  
-       ,CAST(DateRetrieved AS DATETIME) AS DateRetrieved 
-  From CEN.Workspaces
+﻿
+
+
+CREATE View [CEN].[vw_Workspaces]
+AS
+
+WITH DashboardApps AS
+(
+	SELECT Distinct Workspace_ID 
+	FROM CEN.Dashboards
+	WHERE SUBSTRING(TRIM([DisplayName]),1,5) = '[App]'
+)
+,ReportApps as
+(
+	SELECT Distinct Workspace_ID 
+	FROM CEN.Reports
+	WHERE SUBSTRING(TRIM([Name]),1,5) = '[App]'
+)
+,Apps as
+(
+	SELECT DISTINCT Workspace_ID 
+	FROM
+	(
+		SELECT Workspace_ID FROM DashboardApps
+		UNION ALL
+		SELECT Workspace_ID FROM ReportApps
+	) as a
+)
+
+Select  W.[Workspace_Id]  
+       ,[Name]  
+       ,[IsReadOnly]  
+       ,[IsOnDedicatedCapacity]  
+       ,[CapacityId]  
+       ,[Description]  
+       ,[Type]  
+       ,[State]
+       ,[IsOrphaned]  
+       ,[Users]  
+       ,[Reports]  
+       ,[Dashboards]  
+       ,[Datasets]  
+       ,[Dataflows]  
+       ,[Workbooks]  
+	   ,CASE WHEN A.Workspace_ID is NULL THEN 0
+	         ELSE 1
+		END as HasApp
+       ,CAST([DateRetrieved] AS DATETIME2) AS DateRetrieved 
+  From CEN.Workspaces as W
+  LEFT JOIN Apps as A
+  on W.Workspace_Id = A.Workspace_ID
